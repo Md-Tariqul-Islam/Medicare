aa6322abab89856a82daf8fdb1b11c8db675a01b
Md.Akram Hossain <akramhoss392ain@gmail.com>
2021-12-03 00:57:04 +0600

    Menu

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
 echo "hello";
-echo "Tariq";
+echo "Tariq and Akram";
 ?>
 
 
@@ -13,7 +13,10 @@ echo "Tariq";
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <link href="css/custom.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 
     <title>MediCare</title>
   </head>
@@ -23,6 +26,36 @@ echo "Tariq";
     <h1>Home Page</h1>
 
     <span> <a href="login.php">Sign-In</a></span>
+
+    <!-- MENU -->
+    <section class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container">
+
+            <div class="navbar-header">
+                <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                </button>
+
+                <!-- lOGO TEXT HERE -->
+                <a href="index.html" class="navbar-brand"><i class="fa fa-plus-square"></i>Medicare</a>
+            </div>
+
+            <!-- MENU LINKS -->
+            <div class="collapse navbar-collapse custnav">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#top" class="smoothScroll">Home</a></li>
+                    <li><a href="#about" class="smoothScroll">About Us</a></li>
+                    <li><a href="#team" class="smoothScroll">Doctors</a></li>
+                    <li><a href="#news" class="smoothScroll">News</a></li>
+                    <li><a href="#google-map" class="smoothScroll">Contact</a></li>
+                    <li class="appointment-btn"><a href="#appointment">Make an appointment</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </section>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
