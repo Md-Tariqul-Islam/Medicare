ebcf866e734f814b0304c5a9d57ab77ac68a95b6
Md-Tariqul-Islam <1830444@iub.edu.bd>
2021-12-02 22:02:26 +0600

    Add 3 php file

--- login.php
+++ login.php
@@ -1,10 +1,3 @@
-<?php 
-
-echo "hello";
-echo "Tariq";
-?>
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -15,14 +8,14 @@ echo "Tariq";
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>MediCare</title>
+    <title>Login | Page</title>
   </head>
   <body>
 
 
-    <h1>Home Page</h1>
+    <h1>Log-In Page</h1>
 
-    <span> <a href="login.php">Sign-In</a></span>
+    <p>Don't have an account? <a href="register.php">Register Here</a>.</p>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
