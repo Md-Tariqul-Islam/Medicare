391fdcc6690be9fe0c10abe6d30f0e11e2bca3ad
Md-Tariqul-Islam <1830444@iub.edu.bd>
2021-12-09 16:16:28 +0600

    Add header

--- index.php
+++ index.php
@@ -6,22 +6,46 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+     <!-- MAIN CSS -->
+    
+     
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!-- MAIN CSS -->
-    <link rel="stylesheet" href="css/style.css">
+   
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"> -->
+    <link rel="stylesheet" href="css/style.css">
 
     <title>MediCare</title>
 
   </head>
   <body>
-    <span> <i class="fas fa-sign-in-alt"></i><a href="login.php"> Sign-In</a></span>
     <!-- header-section start -->
+     <!-- HEADER -->
+    
+     <h1 class="b">Hello</h1>
+     <header>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 col-sm-5">
+                    <p>Welcome to a Professional Health Care Center</p>
+                </div>
+
+                <div class="col-md-8 col-sm-7 text-align-right">
+                    <span><i class="far fa-envelope"></i> <a href="#">medicare@gmail.com</a></span>
+                    <span> <i class="fas fa-sign-in-alt"></i><a href="login.php"> Sign-In</a></span>
+                </div>
+
+            </div>
+        </div>
+    </header>
+
+
     <header class="header-section">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container">
