a7f79ec36ba329ea0b92515e52be441744892e34
Md-Tariqul-Islam <1830444@iub.edu.bd>
2021-12-13 11:55:55 +0600

    Add icon color and responsive the header

--- index.php
+++ index.php
@@ -6,39 +6,32 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-     <!-- MAIN CSS -->
-    
-     
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time();?>">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-   
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"> -->
-    <link rel="stylesheet" href="css/style.css">
 
     <title>MediCare</title>
 
   </head>
   <body>
-    <!-- header-section start -->
-     <!-- HEADER -->
-    
-     <h1 class="b">Hello</h1>
+     <!-- Header -->
      <header>
-        <div class="container">
-            <div class="row">
+        <div class="container pt-3">
+            <div class="row ">
 
-                <div class="col-md-4 col-sm-5">
-                    <p>Welcome to a Professional Health Care Center</p>
+                <div class="col text-center">
+                    <p class="text-nowrap">Welcome to a Professional Health Care Center</p>
                 </div>
 
-                <div class="col-md-8 col-sm-7 text-align-right">
-                    <span><i class="far fa-envelope"></i> <a href="#">medicare@gmail.com</a></span>
-                    <span> <i class="fas fa-sign-in-alt"></i><a href="login.php"> Sign-In</a></span>
+                <div class="col text-center text-nowrap">
+                    <span class="icon"><i class="far fa-calendar-plus"></i>24/7</span>
+                    <span class="icon"><i class="far fa-envelope"></i> <a href="#">medicare@gmail.com</a></span>
+                    <span class="icon"> <i class="fas fa-sign-in-alt"></i><a href="login.php"> Sign-In</a></span>
                 </div>
 
             </div>
@@ -46,7 +39,7 @@
     </header>
 
 
-    <header class="header-section">
+    <section class="header-section">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container">
             <a class="navbar-brand" href="index.html">
@@ -79,7 +72,7 @@
             </div>
           </div>
         </nav>
-    </header>
+    </section>
     <!-- header-section end -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
