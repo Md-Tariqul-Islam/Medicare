f5460c694e459a6b584cab162d29cc8485c8a5ea
Md.Akram Hossain <akramhoss392ain@gmail.com>
2021-12-14 17:08:48 +0600

    fixed logo colour and add home section

--- index.php
+++ index.php
@@ -74,6 +74,51 @@
         </nav>
     </section>
     <!-- header-section end -->
+
+    <!-- home start  -->
+    <section class="home">
+      <div class="container">
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/s1.jpg" class="d-block w-100" alt="...">
+      <div class="inner-text">
+        <h3>Let's make your life happier & healthier</h3>
+        <h1>Healthy & Secured Life.</h1>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/s2.jpg" class="d-block w-100" alt="...">
+      <div class="inner-text">
+        <h3>Let's make your life happier & healthier</h3>
+        <h1>Perfect Lifestyle</h1>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/s3.jpg" class="d-block w-100" alt="...">
+      <div class="inner-text">
+        <h3>Let's make your life happier & healthier</h3>
+        <h1>Your Health Necessities</h1>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+      </div>
+    </section>
+    <!-- home end  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
