8ec2de79799e4667324eee888f3e42aadbd03e4f
Md-Tariqul-Islam <1830444@iub.edu.bd>
2021-12-17 16:08:00 +0600

    Show all doctor in fontend fatch from database which is add by admin

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 
+<?php 
+  //  database
+  include "dbconnect.php";
+
+  // data select
+  $select_sql = "SELECT * FROM ourdoctor";
+  $allData=mysqli_query($link,$select_sql);
+  //print_r($allData);
+
+?>
+
+
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -202,7 +217,42 @@
     </section>
     <!-- appointment end -->
 
-    
+
+
+  <div class="container mt-4">
+    <div class="row">
+
+      <div class="col-md-12 col-sm-12">     
+        <h2 class="text-center">Our Doctors</h2>    
+      </div>
+      
+
+      <?php 
+      
+      if( mysqli_num_rows($allData)>0){ 
+      while($fetchData=mysqli_fetch_assoc($allData)) {?>
+
+        <div class="col-md-4 col-sm-6 my-3">
+          <div class="card shadow">
+            <img src="<?php echo $fetchData['image'];?>" class="card-img-top" alt="this" id="image">
+            <div class="card-body">
+              <h2><?php echo $fetchData['name'];?></h2>
+              <h6 class="lead"><?php echo $fetchData['department'];?></h6>
+              <p><i class="fas fa-phone"></i> <?php echo $fetchData['phone'];?></p>
+              <p><i class="far fa-envelope"></i> <a href="mailto:<?php echo $fetchData['email'];?>" id="docemail"><?php echo $fetchData['email'];?></a></p>
+            </div>
+          </div>
+        </div>
+
+      <?php 
+         }
+        }else{
+      ?>
+      <div class="col-lg-12">No Doctor to show</div>
+      <?php }?>
+
+    </div>
+  </div>
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
